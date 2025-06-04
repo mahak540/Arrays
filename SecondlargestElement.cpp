f84b743ea30f094c83073edfc8069c7f70ecd751
mahak540 <162689029+mahak540@users.noreply.github.com>
2025-06-04 11:03:26 +0530

    Create SecondlargestElement.cpp

---
+++ SecondlargestElement.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int getSecondLargest(vector<int> &arr) {
+        // Code Here
+        int n=arr.size();
+        if(n<2){
+	return -1;
+        }
+	
+    int large=INT_MIN,second_large=INT_MIN;
+    int i;
+    for (i = 0; i < n; i++) 
+    {
+        if (arr[i] > large) 
+        {
+            second_large = large;
+            large = arr[i];
+        }
+ 
+        else if (arr[i] > second_large && arr[i] != large) 
+        {
+            second_large = arr[i];
+        }
+    }
+    return (second_large == INT_MIN) ? -1 : second_large;      
+    }
+};
